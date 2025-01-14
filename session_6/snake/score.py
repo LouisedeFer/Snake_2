bbe9b17d2e8a9710dec6be09ddd3a4d6ad6198fd
Louise de Ferran <louise.deferran@orange.fr>
2025-01-14 22:27:02 +0100

    Save and load highscores

--- session_6/snake/score.py
+++ session_6/snake/score.py
@@ -1,24 +1,32 @@
 class Score :
     """Define the score with a name and a score (int)."""
 
-    MAX_LENGHT = 8
+    MAX_LENGTH = 8
 
 
     def __init__(self, score : int, name : str) -> None:
         """Initialize."""
         self._score=score
-        self._name=name[:self.MAX_LENGHT]
+        self.name=name #use the property 
 
     @property
     def name(self) -> str :
         """Return the name."""
         return self._name
 
+    @name.setter
+    def name(self, n : str) -> None :
+        """Modify the name."""
+        self._name=n[:self.MAX_LENGTH]
+
+
+
     @property 
     def score(self) -> int :
         """Return the score."""
         return self._score
 
+    
     # Implemente the comparaison operators to use the function sort in the lists
     def __gt__(self, other : object) -> bool :
         """Define the comparaison operator."""
