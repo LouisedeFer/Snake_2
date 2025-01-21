0e4981aceb8be59cb4ad9e4f5afbeefb698d6530
Louise de Ferran <louise.deferran@orange.fr>
2025-01-21 16:17:46 +0100

    info messages

--- session_6/snake/subject.py
+++ session_6/snake/subject.py
@@ -1,9 +1,11 @@
 # ruff: noqa: D100,S311
 
 # First party
+import logging
+
 from .observer import Observer
 
-
+logger = logging.getLogger("foo")
 class Subject:
     """Mother class representing a subject for the Observer pattern."""
 
@@ -19,8 +21,10 @@ class Subject:
 
     def attach_obs(self, obs: Observer) -> None:
         """Attach an observer."""
+        logger.info("Attach an observer")
         self._observers.append(obs)
 
     def detach_obs(self, obs: Observer) -> None:
         """Detach an observer."""
+        logger.info("Detach an observer")
         self._observers.remove(obs)
